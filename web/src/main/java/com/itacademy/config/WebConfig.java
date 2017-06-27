00fb6486fd5aead1b2f1967d464d8d45633f0030
Tom <km.kathrin3@gmail.com>
2017-06-27 14:55:26 +0300

    thymeleaf added. version was tested. workes

--- web/src/main/java/com/itacademy/config/WebConfig.java
+++ web/src/main/java/com/itacademy/config/WebConfig.java
@@ -1,23 +1,14 @@
 package com.itacademy.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
 
 @Configuration
-@ComponentScan(basePackages = "com.itacademy.controller")
 @EnableWebMvc
+@ComponentScan(basePackages = {"com.itacademy.controller"})
+@Import(value = {InternationalizationConfig.class, ThymeleafConfig.class})
 public class WebConfig {
-
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(".jsp");
-        viewResolver.setExposeContextBeansAsAttributes(true);
-        return viewResolver;
-    }
 }
